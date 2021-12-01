645d43a13644a065426da81e015e035f671716a2
Maciej Krzywda <mkrzywda@x230>
2021-12-01 10:36:01 +0100

    Day 1: Sonar Sweep

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    args
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [current, next] -> next > current end)
   end
 
   def part2(args) do
+    args
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [current, next] -> next > current end)
   end
+
 end
